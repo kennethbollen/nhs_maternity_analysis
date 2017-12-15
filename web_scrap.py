f6711588941f277ec5b180206ac6072aa8f7305a
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2017-12-15 23:12:31 +0000

    Update web_scrap.py
    looping through web pages

--- web_scrap.py
+++ web_scrap.py
@@ -3,17 +3,13 @@
 import requests
 import bs4
 
-#access the website
-url = requests.get('https://digital.nhs.uk/article/4375/Public-health')
-url.raise_for_status()
-soup = bs4.BeautifulSoup(url.text)
-
-#find the hyperlinks for the web pages that contain the maternity data
-
 maternity_hyplink = []
-
-for link in soup.find_all('a', href=True):
-    #regex to search html for hyperlinks with the link text for maternity web pages using a wildcard
-    if re.search("Maternity Services Monthly Statistics, England.", link.text) is not None:
-        #for each hyperlink, append to the list maternity_hylink and strip whitespace
-        maternity_hylink.append(link['href'])
+        
+for i in range(1,4):
+	#find the hyperlinks for the web pages that contain the maternity data
+    url = requests.get("https://digital.nhs.uk/article/4375/Public-health?p=%s" %i)
+	url.raise_for_status()
+	soup = bs4.BeautifulSoup(url.text)
+	for link in soup.find_all('a', href=True):
+		if re.search("Maternity Services Monthly Statistics, England.", link.text) is not None:
+			maternity_hylink.append(link['href'])
