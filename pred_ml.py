cc5d6b83795b28bd22f6a5ff9a29028a8674ae1c
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-04-25 21:32:52 +0100

    polyfit
    added deg

--- pred_ml.py
+++ pred_ml.py
@@ -19,7 +19,7 @@ for i in range(len(X_list)):
 #fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10) = plt.subplots(2, 5)
 fig = plt.figure()
 
-slope_smk, int_smk = np.polyfit(smk, pre)
+slope_smk, int_smk = np.polyfit(smk, pre, 1)
 x_smk = np.array([min(smk), max(smk)])
 y_smk = slope_smk * x_smk + int_smk
 plt.subplot(2, 5, 1)
@@ -29,7 +29,7 @@ plt.ylabel('premature babies')
 plt.title('%s correlation' % round(x_corr[0], 2))
 plt.plot(x_smk, y_smk)
 
-slope_under, int_under = np.polyfit(under, pre)
+slope_under, int_under = np.polyfit(under, pre, 1)
 x_under = np.array([min(under), max(under)])
 y_under = slope_under * x_under + int_under
 plt.subplot(2, 5, 2)
@@ -39,7 +39,7 @@ plt.ylabel('premature babies')
 plt.title('%s correlation' % round(x_corr[1], 2))
 plt.plot(x_under, y_under)
 
-slope_obese, int_obese = np.polyfit(obese, pre)
+slope_obese, int_obese = np.polyfit(obese, pre,1)
 x_obese = np.array([min(obese), max(obese)])
 y_obese = slope_obese * x_obese + int_obese
 plt.subplot(2, 5, 3)
@@ -49,7 +49,7 @@ plt.set_ylabel('premature babies')
 plt.set_title('%s correlation' % round(x_corr[2], 2))
 plt.plot(x_obese, y_obese)
 
-slope_age39, int_age39 = np.polyfit(age35_to_39, pre)
+slope_age39, int_age39 = np.polyfit(age35_to_39, pre,1)
 x_age39 = np.array([min(age35_to_39), max(age35_to_39)])
 y_age39 = slope_age39 * x_age39 + int_age39
 plt.subplot(2, 5, 4)
@@ -59,7 +59,7 @@ plt.set_ylabel('premature babies')
 plt.set_title('%s correlation' % round(x_corr[3], 2))
 plt.plot(x_age39, y_age39)
 
-slope_age44, int_age44 = np.polyfit(age40_to_44, pre)
+slope_age44, int_age44 = np.polyfit(age40_to_44, pre,1)
 x_age44 = np.array([min(age40_to_44), max(age40_to_44)])
 y_age44 = slope_age44 * x_age44 + int_age44
 plt.subplot(2, 5, 5)
@@ -69,7 +69,7 @@ plt.set_ylabel('premature babies')
 plt.set_title('%s correlation' % round(x_corr[4], 2))
 plt.plot(x_age44, y_age44)
 
-slope_plus45, int_plus45 = np.polyfit(plus45, pre)
+slope_plus45, int_plus45 = np.polyfit(plus45, pre,1)
 x_plus45 = np.array([min(plus45), max(plus45)])
 y_plus45 = slope_plus45 * x_plus45 + int_plus45
 plt.subplot(2, 5, 6)
@@ -79,7 +79,7 @@ plt.set_ylabel('premature babies')
 plt.set_title('%s correlation' % round(x_corr[5], 2))
 plt.plot(x_plus45, y_plus45)
 
-slope_caesar, int_caesar = np.polyfit(caesar, pre)
+slope_caesar, int_caesar = np.polyfit(caesar, pre,1)
 x_caesar = np.array([min(caesar), max(caesar)])
 y_caesar = slope_caesar * x_caesar + int_caesar
 plt.subplot(2, 5, 7)
@@ -89,7 +89,7 @@ plt.set_ylabel('premature babies')
 plt.set_title('%s correlation' % round(x_corr[6], 2))
 plt.plot(x_caesar, y_caesar)
 
-slope_no_caesar, int_no_caesar = np.polyfit(no_caesar, pre)
+slope_no_caesar, int_no_caesar = np.polyfit(no_caesar, pre,1)
 x_no_caesar = np.array([min(no_caesar), max(no_caesar)])
 y_no_caesar = slope_no_caesar * x_no_caesar + int_no_caesar
 plt.subplot(2, 5, 8)
@@ -99,7 +99,7 @@ plt.set_ylabel('premature babies')
 plt.set_title('%s correlation' % round(x_corr[7], 2))
 plt.plot(x_no_caesar, y_no_caesar)
 
-slope_no_birth, int_no_birth = np.polyfit(no_birth, pre)
+slope_no_birth, int_no_birth = np.polyfit(no_birth, pre,1)
 x_no_birth = np.array([min(no_birth), max(no_birth)])
 y_no_birth = slope_no_birth * x_no_birth + int_no_birth
 plt.subplot(2, 5, 9)
@@ -109,7 +109,7 @@ plt.set_ylabel('premature babies')
 plt.set_title('%s correlation' % round(x_corr[8], 2))
 plt.plot(x_no_birth, y_no_birth)
 
-slope_no_live_birth, int_no_live_birth = np.polyfit(no_live_birth, pre)
+slope_no_live_birth, int_no_live_birth = np.polyfit(no_live_birth, pre,1)
 x_no_live_birth = np.array([min(no_live_birth), max(no_live_birth)])
 y_no_live_birth = slope_no_live_birth * x_no_live_birth + int_no_live_birth
 plt.subplot(2, 5, 10)
